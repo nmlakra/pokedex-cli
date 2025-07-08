3a22cdb202f2f70037eae37e7e6f38f7a7cc594e
nmlakra <nikhilmarklakra@gmail.com>
2025-07-08 21:12:58 +0530

    added exit and help commands

--- main.go
+++ main.go
@@ -7,13 +7,37 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
+
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		inputText := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", inputText[0])
+		if _, ok := commands[inputText[0]]; !ok {
+			fmt.Println("Unkown command")
+		} else {
+			commands[inputText[0]].callback()
+		}
 	}
 }
 
@@ -21,4 +45,18 @@ func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	words := strings.Fields(lowerText)
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Print("Usage:\n\n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
 }
