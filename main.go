77024a785e8ff6ae731f9d93dc39d44ad34e1b72
nmlakra <nikhilmarklakra@gmail.com>
2025-07-09 11:53:58 +0530

    refactor repl logic in main function

--- main.go
+++ main.go
@@ -15,7 +15,28 @@ type cliCommand struct {
 
 func main() {
 
-	commands := map[string]cliCommand{
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		inputTexts := cleanInput(scanner.Text())
+		commandName := inputTexts[0]
+		if command, ok := validCommands()[commandName]; !ok {
+			fmt.Println("Unkown command")
+		} else {
+			command.callback()
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	lowerText := strings.ToLower(text)
+	words := strings.Fields(lowerText)
+	return words
+}
+
+func validCommands() map[string]cliCommand {
+	return map[string]cliCommand{
 		"exit": {
 			name:        "exit",
 			description: "Exit the pokedex",
@@ -27,22 +48,4 @@ func main() {
 			callback:    commandHelp,
 		},
 	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		inputText := cleanInput(scanner.Text())
-		if _, ok := commands[inputText[0]]; !ok {
-			fmt.Println("Unkown command")
-		} else {
-			commands[inputText[0]].callback()
-		}
-	}
-}
-
-func cleanInput(text string) []string {
-	lowerText := strings.ToLower(text)
-	words := strings.Fields(lowerText)
-	return words
 }
